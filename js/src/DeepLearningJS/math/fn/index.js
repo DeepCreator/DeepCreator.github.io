675a40b6f7d8ea073700f3666a8379fe5d2ebf30
DeepCreator <13652311447@126.com>
2018-05-05 19:56:29 +0800

    Site updated: 2018-05-05 19:55:33

--- js/src/DeepLearningJS/math/fn/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  linear:  require('./linear'),
-  lrelu:    require('./lrelu'),
-  relu:    require('./relu'),
-  sigmoid: require('./sigmoid'),
-  softmax: require('./softmax'),
-  tanh:    require('./tanh')
-};
--- js/src/DeepLearningJS/math/operator/flatten.js
+++
@@ -1,5 +0,0 @@
-function flatten(matrix) {
-  return Array.prototype.concat.apply([], matrix);
-}
-
-module.exports = flatten;
