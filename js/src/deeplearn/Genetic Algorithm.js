c96d17d8e9cd020b7877986b16042129cf11f2db
DeepCreator <13652311447@126.com>
2018-06-14 09:19:47 +0800

    Site updated: 2018-06-14 09:18:02

--- js/src/deeplearn/Genetic Algorithm.js
+++ js/src/deeplearn/Genetic Algorithm.js
@@ -1,1 +1,25 @@
-for(var costf=function(o){for(var c=0,e=0;e<14;e++)c+=.5*e*o[e]*Math.exp(-o[e]+o[e+1])/o[e+1];return c+=3*o[14]/o[0]},domain=[],i=0;i<15;i++)domain.push([1,70]);var vec=ml.optimize.genetic({domain:domain,costf:costf,population:50,elite:2,epochs:300,q:.3});console.log("vec : ",vec),console.log("cost : ",costf(vec));
+var costf = function(vec) {
+    var cost = 0;
+    for(var i =0; i<14;i++) { // 15-dimensional vector
+        cost += (0.5*i*vec[i]*Math.exp(-vec[i]+vec[i+1])/vec[i+1])
+    }
+    cost += (3.*vec[14]/vec[0]);
+    return cost;
+};
+
+var domain = [];
+for(var i=0;i<15;i++)
+    domain.push([1,70]); // domain[idx][0] : minimum of vec[idx], domain[idx][1] : maximum of vec[idx].
+
+var vec = ml.optimize.genetic({
+    domain : domain,
+    costf : costf,
+    population : 50,
+    elite : 2, // elitism. number of elite chromosomes.
+    epochs : 300,
+    q : 0.3 // Rank-Based Fitness Assignment. fitness = q * (1-q)^(rank-1)
+            // higher q --> higher selection pressure
+});
+
+console.log("vec : ",vec);
+console.log("cost : ",costf(vec));
