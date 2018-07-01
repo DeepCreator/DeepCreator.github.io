8046f9d2fd63e8726e660a2378846eb544518939
DeepCreator <13652311447@126.com>
2018-07-01 12:33:03 +0800

    Site updated: 2018-07-01 12:31:55

--- js/src/deeplearn/Non-negative matrix factorization.js
+++ js/src/deeplearn/Non-negative matrix factorization.js
@@ -1,11 +1,1 @@
-var matrix = [[22,28],
-              [49,64]];
-
-var result = ml.nmf.factorize({
-    matrix : matrix,
-    features : 3,
-    epochs : 100
-});
-
-console.log("First Matrix : ",result[0]);
-console.log("Second Matrix : ",result[1]);
+var matrix=[[22,28],[49,64]],result=ml.nmf.factorize({matrix:matrix,features:3,epochs:100});console.log("First Matrix : ",result[0]),console.log("Second Matrix : ",result[1]);
